2f920e3aa63778cf5c91134bd947b76a3c1a9f10
Viczián István <viczian.istvan@gmail.com>
2022-09-06 16:11:58 +0200

    while

--- while_user.py
+++ while_user.py
@@ -1,3 +1,12 @@
 # A felhasználótól kérjetek be egy számot, és írjátok ki
 # a kétszeresét. Ezt egészen addig ismételjétek, amíg
 # a felhasználó 0-át nem ír be.
+
+# DRY - don't repeat yourself
+
+#number = int(input("Add meg a számot!"))
+number = 100
+while not number == 0:   
+    number = int(input("Add meg a számot!"))
+    print(number * 2)
+    
