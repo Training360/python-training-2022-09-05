a116a4692c68c090cde37d92982e0c2a03a26f42
Viczián István <viczian.istvan@gmail.com>
2022-09-06 14:50:04 +0200

    Összegzés

--- lists.py
+++ lists.py
@@ -1,3 +1,6 @@
+from re import M
+
+
 names = ["Joe", "Jack", "Jane"]
 
 # végigiterálunk a names változó elemein
@@ -16,9 +19,52 @@ for name in names:
     counter += 2
 
 # 1. Írd ki az első három hónap nevét egymás alá!
-# 2. Írd ki, hogy "Az év egyik hónapja január." az első három hónappal!
+months = ["január", "február", "március"]
+for month in months:
+    print(month)
+
+lista = ["január", "február", "március"]
+for ciklusvaltozo in lista:
+    print(ciklusvaltozo)
+
+ciklusvaltozo = "január"
+print(ciklusvaltozo)
+ciklusvaltozo = "február"
+print(ciklusvaltozo)
+ciklusvaltozo = "március"
+print(ciklusvaltozo)
+
+# 2. Írd ki, hogy "Az év egyik hónapja xxx." az első három hónappal!
+months = ["január", "február", "március"]
+for month in months:
+    print(f"Az év egyik hónapja {month}.")
+
 # 3. Hozz létre egy listát a 3, 7, 9, 13 számokkal! Add össze a listában
 # lévő számokat!
+
+# Írj egy ciklust, amely összeadja a listában szereplő 
+# összes számot egy `sum` változóba! 
+# A `sum` változónak 0 értéket kell adnod az összegzés előtt,
+#  majd a ciklus befejezése után az `sum` változó értékét írasd ki!
+
+numbers = [3, 7, 9, 13]
+sum = 0
+for number in numbers:
+    sum += number
+print(sum)
+
+numbers = [3, 7, 9, 13]
+sum = 0 # 0
+number = 3
+sum += number # 0 + 3 = 3
+number = 7
+sum += number # 3 + 7 = 10
+number = 9
+sum += number # 10 + 9 = 19
+number = 13
+sum += number # 19 + 13 = 32
+print(sum)
+
 
 numbers = [3, 7, 9, 13]
 for number in numbers:
