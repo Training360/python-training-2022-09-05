11b0ee8f1dd7f1b4ef5d38a91fbbb9ead49317a5
Viczián István <viczian.istvan@gmail.com>
2022-09-06 15:59:19 +0200

    boole while

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+# While, azaz elöltesztelő ciklus
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+# Végtelenciklus
+#while True:
+#    print("Hello World!")
+
+# Nem fut le a ciklus törzse
+#while False:
+#    print("One")
+#print("Two")
+
+# Végtelenciklus
+#i = 0
+#while i < 5: # Amíg a feltétel igaz, addig lefuttatja a ciklustörzset, 
+    # ciklusmagot
+#    print(i) 
---
+++ while_user.py
@@ -0,0 +1,3 @@
+# A felhasználótól kérjetek be egy számot, és írjátok ki
+# a kétszeresét. Ezt egészen addig ismételjétek, amíg
+# a felhasználó 0-át nem ír be.
